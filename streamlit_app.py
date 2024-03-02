9915d84414eeebd05f22414a60c2a61ea744a327
Rigvedsarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2024-03-02 13:38:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,13 @@ with st.sidebar:
 
     year_list = list(df_reshaped.year.unique())[::-1]
 
-    selected_year = st.selectbox('Select a year', year_list)
-    df_selected_year = df_reshaped[df_reshaped.year == selected_year]
-    df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
+    selected_start_year = st.selectbox('Select start year', year_list)
+    selected_end_year = st.selectbox('Select end year', year_list)
+
+    if selected_start_year > selected_end_year:
+        st.error("Start year must be before end year.")
+    else:
+        df_selected_years = df_reshaped[(df_reshaped.year >= selected_start_year) & (df_reshaped.year <= selected_end_year)]
 
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
@@ -61,7 +65,7 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
 def make_choropleth(input_df, input_id, input_column, input_color_theme):
     choropleth = px.choropleth(input_df, locations=input_id, color=input_column, locationmode="USA-states",
                                color_continuous_scale=input_color_theme,
-                               range_color=(0, max(df_selected_year.population)),
+                               range_color=(0, max(df_selected_years.population)),
                                scope="usa",
                                labels={'population':'Population'}
                               )
@@ -152,9 +156,9 @@ col = st.columns((1.5, 4.5, 2), gap='medium')
 with col[0]:
     st.markdown('#### Gains/Losses')
 
-    df_population_difference_sorted = calculate_population_difference(df_reshaped, selected_year)
+    df_population_difference_sorted = calculate_population_difference(df_selected_years, selected_end_year)
 
-    if selected_year > 2010:
+    if selected_end_year > 2010:
         first_state_name = df_population_difference_sorted.states.iloc[0]
         first_state_population = format_number(df_population_difference_sorted.population.iloc[0])
         first_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[0])
@@ -164,7 +168,7 @@ with col[0]:
         first_state_delta = ''
     st.metric(label=first_state_name, value=first_state_population, delta=first_state_delta)
 
-    if selected_year > 2010:
+    if selected_end_year > 2010:
         last_state_name = df_population_difference_sorted.states.iloc[-1]
         last_state_population = format_number(df_population_difference_sorted.population.iloc[-1])
         last_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[-1])
@@ -177,7 +181,7 @@ with col[0]:
 
     st.markdown('#### States Migration')
 
-    if selected_year > 2010:
+    if selected_end_year > 2010:
         # Filter states with population difference > 50000
         # df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference_absolute > 50000]
         df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference > 50000]
@@ -204,20 +208,20 @@ with col[0]:
 with col[1]:
     st.markdown('#### Total Population')
 
-    choropleth = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)
+    choropleth = make_choropleth(df_selected_years, 'states_code', 'population', selected_color_theme)
     st.plotly_chart(choropleth, use_container_width=True)
 
-    heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
+    heatmap = make_heatmap(df_selected_years, 'year', 'states', 'population', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
 
     # Linear trend graph
-    linear_trend = make_linear_trend(df_selected_year, 'year', 'population')
+    linear_trend = make_linear_trend(df_selected_years, 'year', 'population')
     st.altair_chart(linear_trend, use_container_width=True)
 
 with col[2]:
     st.markdown('#### Top States')
 
-    st.dataframe(df_selected_year_sorted,
+    st.dataframe(df_selected_years_sorted,
                  column_order=("states", "population"),
                  hide_index=True,
                  width=None,
@@ -229,7 +233,7 @@ with col[2]:
                         "Population",
                         format="%f",
                         min_value=0,
-                        max_value=max(df_selected_year_sorted.population),
+                        max_value=max(df_selected_years_sorted.population),
                      )}
                  )
 
